2d50dbec496f58481985164febe5f7aa80c54129
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-09 16:56:22 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+const server = app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+module.exports = server;
