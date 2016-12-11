3afe1a1375d097bcd61fddf49b0c8a0242e429eb
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-11 10:10:29 -0500

    commented out mock data

--- dreamsRouter.js
+++ dreamsRouter.js
@@ -5,11 +5,11 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 const {dreams} = require('./models');
-
+/*
 dreams.create('test title 1', 'lorem ipsum', 'lucid', 8);
 dreams.create('test title 2', 'morem jpsum', 'lucid', 8);
 dreams.create('test title 3', 'norem kpsum', 'lucid', 8);
-
+*/
 router.get('/', (req, res) => {
   res.json(dreams.get());
 });
