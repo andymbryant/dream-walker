eb0cb3093d889f72595b7a8cbe6ef19140c6ffe8
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-10 19:17:52 -0500

    added new morgan instance

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const morgan = require('morgan')
+const app = express();
+
 app.use(express.static('public'));
+app.use(morgan('common'));
 const server = app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(__dirname + '/public/dashboard.html');
+});
+
+app.get('/dreams', (req, res) => {
+  res.sendFile(__dirname + '/public/dreams.html');
 });
 
 module.exports = server;
