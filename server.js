76abd990896b97d766c6b80ebc83c5e6448cc51d
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-12 17:03:00 -0500

    added dashboardRouter

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const morgan = require('morgan')
 const app = express();
 const indexRouter = require('./indexRouter');
 const dreamsRouter = require('./dreamsRouter');
-
+const dashboardRouter = require('./dashboardRouter');
 
 app.use(express.static('public'));
 app.use(morgan('common'));
+
 const server = app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
-
-app.get('/dashboard', (req, res) => {
-  res.sendFile(__dirname + '/public/dashboard.html');
 });
 
 app.get('/new-dream', (req, res) => {
@@ -20,6 +17,8 @@ app.get('/new-dream', (req, res) => {
 });
 
 app.use('/', indexRouter);
+
+app.use('/dashboard', dashboardRouter);
 
 app.use('/dreams', dreamsRouter);
 
