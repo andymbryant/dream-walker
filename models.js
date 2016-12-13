1e37267b307d8627770ac54b0918efcb153cef56
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-12 22:53:48 -0500

    added users model

--- models.js
+++ models.js
@@ -38,7 +38,44 @@ const dreams = {
   }
 };
 
+const users = {
+  create: function(username, password) {
+    console.log('Creating a new user...');
+    const item = {
+      id: uuid.v4(),
+      username: username,
+      password: password
+    };
+    this.items[item.id] = item;
+    return item;
+  },
+  get: function() {
+    console.log('Retrieving all users...');
+    return Object.keys(this.items).map(key => this.items[key]);
+  },
+  delete: function(id) {
+    console.log(`Deleting user \`${id}\``);
+    delete this.items[id];
+  },
+  update: function(updatedItem) {
+    console.log(`Updating user \`${updatedItem.id}\``);
+    const {id} = updatedItem;
+    if (!(id in this.items)) {
+      throw StorageException(
+        `Can't update user \`${id}\` because doesn't exist.`)
+    }
+    this.items[updatedItem.id] = updatedItem;
+    return updatedItem;
+  }
+};
+
 function createDreamEntry() {
+  const storage = Object.create(dreams);
+  storage.items = {};
+  return storage;
+}
+
+function createUser() {
   const storage = Object.create(dreams);
   storage.items = {};
   return storage;
@@ -46,4 +83,5 @@ function createDreamEntry() {
 
 module.exports = {
   dreams: createDreamEntry()
+  users: createUser();
 }
