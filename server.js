20019f7d60f85807ebf1c91f5daa882e1d22c9d0
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-17 16:15:16 -0500

    added server functions

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+const { DATABASE_URL, PORT } = require('./config');
+const { Dream } = require('./models'); 
+
 const app = express();
+
+app.use(morgan('common'));
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+
 const dreamsRouter = require('./dreamsRouter');
 const usersRouter = require('./usersRouter');
 
-app.use(express.static('public'));
-app.use(morgan('common'));
 
-const server = app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
@@ -22,4 +28,54 @@ app.use('/users', usersRouter);
 app.use('/dreams', dreamsRouter);
 
 
-module.exports = server;
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
+
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server;
+
+// this function connects to our database, then starts the server
+function runServer() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(DATABASE_URL, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(PORT, () => {
+        console.log(`Your app is listening on port ${PORT}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
