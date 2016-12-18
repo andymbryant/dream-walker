5ae20e40b3d8d8425362c70b81c77504c1a8c08c
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-18 13:13:30 -0500

    added static file for GET request to root

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.use(express.static('public'));
 
 mongoose.Promise = global.Promise;
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.get('/dreams', (req, res) => {
   Dream
