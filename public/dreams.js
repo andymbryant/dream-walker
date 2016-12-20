7ec17960127a30cb0ab2a9ee1fb7852f7b93356f
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-20 14:35:06 -0500

    styling and position for dream accordion

--- public/dreams.js
+++ public/dreams.js
@@ -76,8 +76,8 @@ function displayDreamEntries(data) {
         '<dt><p class="dream-title">' + data[index].title + '</p>' +
         '<p class="dream-date">' + data[index].created + '</p></dt>' +
         '<dd><blockquote>' + data[index].entry + '</blockquote>' +
-        '<p class="dream-info">Hours Slept: ' + data[index].hoursSlept + '</p>' +
-        '<p class="dream-info">Dream Type: ' + data[index].type + '</p>' +
+        '<p class="dream-info">Hours Slept: ' + '<span class="stats">' + data[index].hoursSlept + '</span></p>' +
+        '<p class="dream-info">Dream Type: ' + '<span class="stats">' + data[index].type + '</span></p>' +
         '<p class="dream-info"><a href="#" class="edit-icon"><i class="fa fa-pencil fa-lg" aria-hidden="true"></i> Edit</a></p>' +
         '</dd>'
         );
