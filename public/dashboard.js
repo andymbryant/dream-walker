0cfab16259da39c2ea59923dc42f592fec22b021
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-22 15:43:58 -0500

    added line + doughnut chart placeholders

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,14 +1,17 @@
 const LINE_CHART = $("#lineChart");
-console.log(lineChart);
-let sleepChart = new Chart(LINE_CHART, {
+const DOUGHNUT_CHART = $("#doughnutChart");
+
+Chart.defaults.global.animation.duration = 2500;
+
+const sleepChart = new Chart(LINE_CHART, {
   type: 'line',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ["12.01.16", "12.02.16", "12.03.16", "12.04.16", "12.05.16", "12.06.16", "12.07.16", "12.08.16", "12.09.16", "12.10.16", "12.11.16", "12.12.16", "12.13.16", "12.14.16"],
     datasets: [
       {
-        label: "My First dataset",
-        fill: false,
-        lineTension: 0.1,
+        label: "Hours Slept",
+        fill: true,
+        lineTension: 0,
         backgroundColor: "rgba(75,192,192,0.4)",
         borderColor: "rgba(75,192,192,1)",
         borderCapStyle: 'butt',
@@ -17,15 +20,38 @@ let sleepChart = new Chart(LINE_CHART, {
         borderJoinStyle: 'miter',
         pointBorderColor: "rgba(75,192,192,1)",
         pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
+        pointBorderWidth: 3,
         pointHoverRadius: 5,
         pointHoverBackgroundColor: "rgba(75,192,192,1)",
         pointHoverBorderColor: "rgba(220,220,220,1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
+        pointHoverBorderWidth: 3,
+        pointRadius: 5,
         pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40],
+        data: [7, 9, 6, 8, 8, 5, 7, 3, 9, 8, 7, 7, 8, 6],
         spanGaps: false,
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+
+const dreamChart = new Chart(DOUGHNUT_CHART, {
+  type: 'doughnut',
+  data: {
+    labels: ['Normal', 'Lucid', 'Nightmare', 'Recurring', 'Double'],
+    datasets:[
+      {
+        label: 'Points',
+        backgroundColor: ['#54c6ff', '#ff7ae0', '#bc7aff', '#fffc7a', '#7afffc'],
+        data:[10, 20, 55, 30, 10]
       }
     ]
   }
