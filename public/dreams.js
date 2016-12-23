eb69d21ccc0cb44a699dd072f194a9c7707d90ce
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-23 14:21:34 -0500

    added id to each dream

--- public/dreams.js
+++ public/dreams.js
@@ -29,12 +29,12 @@ function displayDreamEntries(data) {
   else {
     for (index in data) {
       accordion.append(
-        '<dt><p class="dream-title">' + data[index].title + '</p>' +
+        '<dt id="'+ data[index].id +'"><p class="dream-title">' + data[index].title + '</p>' +
         '<p class="dream-date">' + data[index].created + '</p></dt>' +
         '<dd><blockquote>' + data[index].entry + '</blockquote>' +
         '<p class="dream-info">Hours Slept: ' + '<span class="stats">' + data[index].hoursSlept + '</span></p>' +
         '<p class="dream-info">Dream Type: ' + '<span class="stats">' + data[index].type + '</span></p>' +
-        '<p class="dream-info"><a href="#" class="edit-icon"><i class="fa fa-pencil fa-lg" aria-hidden="true"></i> Edit</a></p>' +
+        '<p class="dream-info"><a href="dreams/'+ data[index].id +'" class="edit-icon"><i class="fa fa-pencil fa-lg" aria-hidden="true"></i> Edit</a></p>' +
         '<p class="dream-info"><a href="#" class="edit-icon"><i class="fa fa-eraser fa-lg" aria-hidden="true"></i> Delete</a></p>' +
         '</dd>'
         );
