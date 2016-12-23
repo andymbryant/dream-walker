122b30c27f1fbc03d3d086b134c7b0c66faa4d02
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-23 16:02:50 -0500

    radio button correctly auto selects from api

--- public/dream-edit.js
+++ public/dream-edit.js
@@ -1,6 +1,6 @@
 function getDreamEntries(callbackFn) {
   $.ajax({
-    url: "/dreams/585d5338b41a4f62ec044af6",
+    url: "/dreams/585d5338b41a4f62ec044af1",
     type: 'GET',
     dataType: 'json',
 
@@ -17,6 +17,38 @@ function getDreamEntries(callbackFn) {
   });
 }
 
+function displayDream(data) {
+  console.log(data);
+  let dreamType;
+  if (data.type === "Normal") {
+    dreamType = '0';
+  }
+  else if (data.type === "Lucid") {
+    dreamType = '1';
+  }
+  else if (data.type === "Nightmare") {
+    dreamType = '2';
+  }
+  else if (data.type === "Recurring") {
+    dreamType = '3';
+  }
+  else if (data.type === "Double") {
+    dreamType = '4';
+  }
+  else {
+    dreamType = '';
+  }
+
+  $('#dream-title').val(data.title);
+  $('.dream-entry').val(data.entry);
+  $('.hours-slept-input').val(data.hoursSlept);
+  $('.dream-type-box').find(':radio[name=type-of-dream][value='+ dreamType +']').prop('checked', true);
+  $('.date-month').val(data.created.split('.')[0]);
+  $('.date-day').val(data.created.split('.')[1]);
+  $('.date-year').val(data.created.split('.')[2]);
+}
+
+
 $('.date-day').change(function(event) {
   if(parseInt(this.value) < 10) {
     this.value ='0'+ this.value;
@@ -28,18 +60,6 @@ $('.date-month').change(function(event) {
     this.value ='0'+ this.value;
   };
 });
-
-
-function displayDream(data) {
-  console.log(data);
-
-  $('#dream-title').val(data.title);
-  $('.dream-entry').val(data.entry);
-  $('.hours-slept-input').val(data.hoursSlept);
-  $('.date-month').val(data.created.split('.')[0]);
-  $('.date-day').val(data.created.split('.')[1]);
-  $('.date-year').val(data.created.split('.')[2]);
-}
 
 
 $(function() {
