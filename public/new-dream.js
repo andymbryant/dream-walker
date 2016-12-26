c74a147ec711a1d0d8882ac7eac718a51dbd5c94
NOUX-NOVA <peterkim.mh@gmail.com>
2016-12-25 22:12:03 -0500

    added comments and fixed spacing

--- public/new-dream.js
+++ public/new-dream.js
@@ -1,8 +1,18 @@
+/* ================================= USER INPUT CONTROL FOR DATES ================================= */
+
+$('.date-month').change(function(event) {
+  if(parseInt(this.value) < 10) {
+    this.value ='0'+ this.value;
+  };
+});
+
 $('.date-day').change(function(event) {
   if(parseInt(this.value) < 10) {
     this.value ='0'+ this.value;
   };
 });
+
+/* ================================= RESPONSIVE NAV ================================= */
 
 $('.handle').on('click', function(event) {
   $('nav ul').toggleClass('showing');
