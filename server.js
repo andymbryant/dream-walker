cc85980dc609a42853f4ba249ae45bbadfcd4be1
Peter Kim <peterkim.mh@gmail.com>
2018-02-27 20:40:03 -0500

    testing

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 
-console.log('herro')
+app.get('/', (req, res) => {
+  res.send('herro world');
+})
 
 app.listen(port, () => {
   console.log(`The magic happens on port ${port}`);
